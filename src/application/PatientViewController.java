4fcc1c98730ada4b3bc7669cd9784be5f3c577f0
tomas <tomasbrivas3@gmail.com>
2024-04-03 14:45:02 -0700

    patientview added

---
+++ src/application/PatientViewController.java
@@ -0,0 +1,48 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+public class PatientViewController {
+
+    @FXML private Button categoryAllButton;
+    @FXML private Button categoryCurrentButton;
+    @FXML private Button categoryPreviousButton;
+    
+    @FXML private Label allCount;
+    @FXML private Label currentCount;
+    @FXML private Label previousCount;
+    
+	//Handle back button (goes home)
+    public void previousScene(ActionEvent event) throws Exception {
+        loadScene("/FXML/role_selection.fxml", event);
+    }
+    
+	//Handle logout button 
+    public void logout(ActionEvent event) throws Exception {
+        loadScene("/FXML/role_selection.fxml", event);
+    }
+    
+    //Method to load the scene
+    private void loadScene(String fxmlFile, ActionEvent event) throws Exception {
+    	Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+    	Parent root = loader.load();
+    	loader.getController();
+    	stage.setScene(new Scene(root, 800, 600));
+    	stage.show();
+    }
+}
