53ec1fe660fdc2b539a058c53beb1773b49e1f77
tomas <tomasbrivas3@gmail.com>
2024-04-05 13:29:07 -0700

    button effects and press effects

--- src/application/PatientViewController.java
+++ src/application/PatientViewController.java
@@ -1,5 +1,6 @@
 package application;
 
+import javafx.beans.property.ObjectProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,22 +10,22 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.event.ActionEvent;
 
 public class PatientViewController {
 
     @FXML private Button categoryAllButton;
     @FXML private Button categoryCurrentButton;
-    @FXML private Button categoryPreviousButton;
     
-    @FXML private Label allCount;
-    @FXML private Label currentCount;
-    @FXML private Label previousCount;
     
 	//Handle back button (goes home)
     public void previousScene(ActionEvent event) throws Exception {
@@ -35,6 +36,74 @@ public class PatientViewController {
     public void logout(ActionEvent event) throws Exception {
         loadScene("/FXML/role_selection.fxml", event);
     }
+    
+    @FXML
+    private void messageButton(ActionEvent event) {
+        event.consume();
+        System.out.println("message button");
+    }
+    
+    @FXML
+    private void newAppointmentButton(ActionEvent event) {
+        event.consume();
+        System.out.println("new appointment button");
+    }
+    
+    @FXML
+    private void settingButton(ActionEvent event) {
+        event.consume();
+        System.out.println("setting button");
+    }
+    
+    @FXML
+    private void comingAppointmentButton(ActionEvent event) {
+        event.consume();
+        System.out.println("coming appointment button");
+    }
+    
+    @FXML
+    private void recordsButton(ActionEvent event) {
+        event.consume();
+        System.out.println("records button");
+    }
+    
+    @FXML
+    private void billingButton(ActionEvent event) {
+        event.consume();
+        System.out.println("billing button");
+    }
+    
+    @FXML
+    private void orderRefillButton(ActionEvent event) {
+        event.consume();
+        System.out.println("order refill button");
+    }
+    
+    @FXML
+    private void notificationButton(ActionEvent event) {
+        event.consume();
+        System.out.println("notifications button");
+    }
+    
+    @FXML
+    private void appointmentButton(ActionEvent event) {
+        event.consume();
+        System.out.println("appointment button");
+    }
+    
+    @FXML
+    private void aboutUsButton(ActionEvent event) {
+        event.consume();
+        System.out.println("about us button");
+    }
+    
+    @FXML
+    private void refillButton(ActionEvent event) {
+        event.consume();
+        System.out.println("refill button");
+    }
+    
+    //-------------------------------
     
     //Method to load the scene
     private void loadScene(String fxmlFile, ActionEvent event) throws Exception {
