9d47bafb052c88a88ef6932b8d69f4c1a52150b1
Kevin J <kev9180@gmail.com>
2024-04-05 22:39:54 -0700

    Remove unused imports

--- src/application/patientMessageBoardController.java
+++ src/application/patientMessageBoardController.java
@@ -8,12 +8,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class patientMessageBoardController {
