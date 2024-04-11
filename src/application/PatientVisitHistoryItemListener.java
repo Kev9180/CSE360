276cea2a087e14399d7229bd99f2cf8b791bf98b
angelvegajr. <angelvegajr.@localhost>
2024-04-10 18:23:46 -0700

    PatientVisitHistoryItemListener comments and understanding

--- src/application/PatientVisitHistoryItemListener.java
+++ src/application/PatientVisitHistoryItemListener.java
@@ -1,5 +1,8 @@
 package application;
 
+// Interface for handling actions on patient visit history items in the UI
 public interface PatientVisitHistoryItemListener {
-	void onItemClick(Patient patient, Visit visit);
+    
+    // Method signature for handling the click event on a visit item, passing the associated patient and visit
+    void onItemClick(Patient patient, Visit visit);
 }
