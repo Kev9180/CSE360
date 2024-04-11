4edc5b062e206f0ec1b3af33e750c189afc05d25
angelvegajr. <angelvegajr.@localhost>
2024-04-10 18:22:09 -0700

    PatientImmunizationItemListener comments

--- src/application/PatientImmunizationItemListener.java
+++ src/application/PatientImmunizationItemListener.java
@@ -1,5 +1,7 @@
 package application;
-
+//Interface for handling actions on patient immunization items in the UI
 public interface PatientImmunizationItemListener {
+	// Method signature for handling the deletion of an item at the specified index
 	public void onDeleteItem(int index);
 }
+  
