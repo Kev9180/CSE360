f89bd6ea3526807909c8f84fee01d98a8512a652
angelvegajr. <angelvegajr.@localhost>
2024-04-10 18:21:01 -0700

    PatientImmunizationItemController comments

--- src/application/PatientImmunizationItemController.java
+++ src/application/PatientImmunizationItemController.java
@@ -6,7 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
-
+//Controller class for the individual items representing patient immunizations in the UI
 public class PatientImmunizationItemController {
 	
     @FXML private Label dateAdministered;
@@ -18,6 +18,7 @@ public class PatientImmunizationItemController {
     private List<String> immunizations;
     private int listIndex;
     
+ // Method to set the labels of the UI elements based on the provided immunization information
     public void setLabels(List<String> immunizations, int listIndex) {
     	this.immunizations = immunizations;
     	this.listIndex = listIndex;
@@ -27,14 +28,15 @@ public class PatientImmunizationItemController {
 		dateAdministered.setText(nameAndDate[1]);
     }
     
+ // Method to set the listener for handling actions on the item
     public void setListener(PatientImmunizationItemListener listener) {
         this.listener = listener;
     }
-    //
+ // Method to get the labels of the UI elements as a concatenated string
     public String getLabels() {
     	return immunizationName.getText() + "|" + dateAdministered.getText();
     }
-
+ // Method to handle the action of deleting the item
     @FXML
     void handleDeleteItem(ActionEvent event) {
     	listener.onDeleteItem(listIndex);
